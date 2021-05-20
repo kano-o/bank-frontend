32f6bd8b187087f8605525e92bdda0c1bbca755a
EvilGenius3 <spilleke.moritz@gmail.com>
2021-05-20 16:50:25 +0200

    add transactioncard

---
+++ src/app/meinkonto/meinkonto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meinkonto',
+  templateUrl: './meinkonto.component.html',
+  styleUrls: ['./meinkonto.component.css']
+})
+export class MeinkontoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
