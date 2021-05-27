d6247f49a8f6a5fa9b9fd267f0e8314196fbf35c
EvilGenius3 <spilleke.moritz@gmail.com>
2021-05-27 22:38:58 +0200

    register fix

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {CookieService} from "ngx-cookie";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient, private cookieService: CookieService) {
+  }
+
+  packet = {
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    phoneNumber: "",
+    password: ""
+  };
+
+  postReg(firstName: string, lastName: string, emailAddress: string, phoneNumber: string, password: string) {
+    this.packet.firstName = firstName;
+    this.packet.lastName = lastName;
+    this.packet.emailAddress = emailAddress;
+    this.packet.phoneNumber = phoneNumber;
+    this.packet.password = password;
+    let header = new HttpHeaders().set("Content-type", "application/json");
+    this.http.post("https://backend.yab-banking.tech/account/register", this.packet, {
+      headers: header,
+      responseType: "text"
+    }).subscribe(
+      (response) => {
+        console.log(response);
+       // window.location.href = ""
+      }
+    );
+  }
 
   ngOnInit(): void {
+
   }
 
 }
